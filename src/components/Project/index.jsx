dde1663c0afc038d09428bec224967b405b486d4
lliam-mcbean <lliam.mcbean@gmail.com>
2024-05-14 11:27:04 -0700

    trigger site build

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Project() {
   return (
     <div className='cursor-pointer w-full h-40 bg-white border rounded-lg mt-2'>
-      
+      making changes to trigger new site build
     </div>
   )
 }
